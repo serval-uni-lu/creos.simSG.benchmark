fefc3f6c51dca3162c67b1cdbbcbc46ad698f33e
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-09-08 09:11:15 +0200

    PDF with embedded fonts + bigger font size

--- results/2020-06/efficientValidProp/considerationValidConf/boxPlotVarLoadSC.R
+++ results/2020-06/efficientValidProp/considerationValidConf/boxPlotVarLoadSC.R
@@ -10,26 +10,28 @@ xVal = sort(unique(xpData$combined))
 idxNaives = grepl("naive", xVal, TRUE)
 colorsBoxes = ifelse(idxNaives, "gray50", "gray80")
 
-par(mar=c(4, 4, 2, 0))
+par(mgp=c(2.5,1,0), mar=c(4,4,2,0))
 boxplot(xpData$stdLoad~xpData$combined,
         col=colorsBoxes,
         ylab = "Standard deviation of cable load",
         xlab = "Cable",
         xaxt='n',
         # names = c("1", "", "2", "", "3", "", "4", "", "5", "", "6", "", "7", "", "8", "", "9", "", "10", "", "11", "", "12", "", "13", "", "14", "", "15", ""),
-        # las = 2
+        # las = 2,
+        cex.lab=1.5,
+        cex.axis=1.5
         )
 
 legend("topleft", 
        legend = c("Only valid configurations","All configurations") , 
        col = colorsBoxes , 
        bty = "n", 
-       pch=20 , 
+       pch=20, 
        pt.cex = 3, 
-       cex = 1, 
+       cex = 1.5, 
        horiz = TRUE, 
-       inset = c(0.2, -0.11),
-       xpd = TRUE
+       inset = c(0, -0.14),
+       xpd = TRUE,
 )
 
 abline(v=seq(from=2.5, to=28.5, by=2), col="grey", lty=2)
@@ -40,5 +42,5 @@ abline(v=seq(from=2.5, to=28.5, by=2), col="grey", lty=2)
 #      adj=0
 #      )
 
-mtext(text=1:9, side = 1, adj = 0, at= c(seq(from=1.3, by=2, length.out = 9)))
-mtext(text=10:15, side = 1, adj = 0, at= c(seq(from=19, by=2, length.out = 6)))
+mtext(text=1:9, side = 1, adj = 0, at= c(seq(from=1.2, by=2, length.out = 9)), cex = 1.5, line = 0.5)
+mtext(text=10:15, side = 1, adj = 0, at= c(seq(from=18.6, by=2.05, length.out = 6)), cex = 1.5, line = 0.5)
